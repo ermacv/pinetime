61d4104e0598597413d55049e005d9dd78fafa36
ermacvla <ermachenkov.v.s@gmail.com>
2023-08-02 00:15:38 +0300

    commit

--- src/main.c
+++ src/main.c
@@ -1,19 +1,16 @@
-/* main.c - Application main entry point */
-
 /*
- * Copyright (c) 2015-2016 Intel Corporation
+ * Copyright (c) 2018 Jan Van Winkel <jan.van_winkel@dxplore.eu>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/types.h>
-#include <stddef.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
+#include <lvgl.h>
+#include <stdio.h>
 #include <string.h>
-#include <errno.h>
-#include <inttypes.h>
-#include <stdint.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/sys/byteorder.h>
 #include <zephyr/kernel.h>
 
 #include <zephyr/device.h>
@@ -26,6 +23,10 @@
 #include <zephyr/bluetooth/services/bas.h>
 #include <zephyr/bluetooth/services/hrs.h>
 #include <zephyr/sys/util.h>
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
 
 #include "battery.h"
 
@@ -55,6 +56,33 @@ static const struct bt_data ad[] = {
 		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
 		      BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))
 };
+
+static uint32_t count;
+
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+#ifdef CONFIG_GPIO
+static struct gpio_dt_spec button_gpio = GPIO_DT_SPEC_GET_OR(
+		DT_ALIAS(sw0), gpios, {0});
+static struct gpio_callback button_callback;
+
+static void button_isr_callback(const struct device *port,
+				struct gpio_callback *cb,
+				uint32_t pins)
+{
+	ARG_UNUSED(port);
+	ARG_UNUSED(cb);
+	ARG_UNUSED(pins);
+
+	count = 0;
+}
+#endif
 
 
 static void connected(struct bt_conn *conn, uint8_t err)
@@ -91,7 +119,80 @@ static struct bt_conn_auth_cb auth_cb_display = {
 
 int main(void)
 {
-	int err = bt_enable(NULL);
+	int ret, err;
+	char count_str[11] = {0};
+	const struct device *display_dev;
+	lv_obj_t *hello_world_label;
+	lv_obj_t *count_label;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device not ready, aborting test");
+		return 0;
+	}
+
+#ifdef CONFIG_GPIO
+	if (device_is_ready(button_gpio.port)) {
+		int err;
+
+		err = gpio_pin_configure_dt(&button_gpio, GPIO_INPUT);
+		if (err) {
+			LOG_ERR("failed to configure button gpio: %d", err);
+			return 0;
+		}
+
+		gpio_init_callback(&button_callback, button_isr_callback,
+				   BIT(button_gpio.pin));
+
+		err = gpio_add_callback(button_gpio.port, &button_callback);
+		if (err) {
+			LOG_ERR("failed to add button callback: %d", err);
+			return 0;
+		}
+
+		err = gpio_pin_interrupt_configure_dt(&button_gpio,
+						      GPIO_INT_EDGE_TO_ACTIVE);
+		if (err) {
+			LOG_ERR("failed to enable button callback: %d", err);
+			return 0;
+		}
+	}
+#endif
+
+	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN)) {
+		lv_obj_t *hello_world_button;
+
+		hello_world_button = lv_btn_create(lv_scr_act());
+		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, 0);
+		hello_world_label = lv_label_create(hello_world_button);
+	} else {
+		hello_world_label = lv_label_create(lv_scr_act());
+	}
+
+	lv_label_set_text(hello_world_label, "Hello world!");
+	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+
+	count_label = lv_label_create(lv_scr_act());
+	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+	lv_task_handler();
+	display_blanking_off(display_dev);
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_set_dt(&led, 1);
+	if (ret < 0) {
+		return 0;
+	}
+
+  err = bt_enable(NULL);
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
 		return 0;
@@ -116,7 +217,14 @@ int main(void)
 		return 0;
 	}
 
-	while (true) {
+	while (1) {
+		if ((count % 100) == 0U) {
+			sprintf(count_str, "%d", count/100U);
+			lv_label_set_text(count_label, count_str);
+		}
+		lv_task_handler();
+		++count;
+
 		int batt_mV = battery_sample();
 
 		if (batt_mV < 0) {
@@ -138,7 +246,7 @@ int main(void)
 
 		/* Battery level simulation */
 		bt_bas_set_battery_level(percentage);
-		k_sleep(K_SECONDS(60));
+
+		k_sleep(K_MSEC(10));
 	}
-	return 0;
 }
